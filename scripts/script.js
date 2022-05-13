2b52b515d4dc918ddfbef4159601c35ab8650387
Hansi Brahimasi <hansibrahimasi@gmail.com>
2022-05-13 13:04:14 +0200

    Clean up script file

--- scripts/script.js
+++ scripts/script.js
@@ -80,19 +80,3 @@ window.onload = function () {
   };
   next();
 };
-
-// Projects carousel slider
-new Siema({
-  selector: ".siema",
-  duration: 200,
-  easing: "ease-out",
-  perPage: 1,
-  startIndex: 0,
-  draggable: true,
-  multipleDrag: false,
-  threshold: 20,
-  loop: true,
-  rtl: false,
-  onInit: () => {},
-  onChange: () => {},
-});
